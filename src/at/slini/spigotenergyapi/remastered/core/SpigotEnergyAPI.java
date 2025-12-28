6b2f6e2551aa7cb34303fe29857a202d6047705c
tamashii <terrocraft3223@gmail.com>
2025-12-28 18:46:14 +0100

    First Init. I think we must reformat the config, and remove Deprecated code.

--- src/at/slini/spigotenergyapi/remastered/core/SpigotEnergyAPI.java
+++ src/at/slini/spigotenergyapi/remastered/core/SpigotEnergyAPI.java
@@ -3,23 +3,22 @@ package at.slini.spigotenergyapi.remastered.core;
 import at.slini.spigotenergyapi.remastered.api.BlockWrapperInstance;
 //import at.slini.spigotenergyapi.remastered.api.spigotenergyapi.Blocks.Managers.BlockWrapperManager;
 import at.slini.spigotenergyapi.remastered.core.Blocks.Managers.BlockWrapperManager;
-import at.slini.spigotenergyapi.remastered.core.Bootstrap.ExternalLibraryBootstrap;
 import at.slini.spigotenergyapi.remastered.core.Commands.GetCustomBlockData;
 import at.slini.spigotenergyapi.remastered.core.Listeners.BlockListener;
 import at.slini.spigotenergyapi.remastered.core.Managers.ModuleRegistry;
 import at.slini.spigotenergyapi.remastered.core.Network.SteamNetworkEngine;
 import at.slini.spigotenergyapi.remastered.core.Utils.ConfigUtil;
 import at.slini.spigotenergyapi.remastered.core.Utils.EnergyBlockUpdate;
-import at.slini.spigotenergyapi.remastered.core.Utils.PlayerActionBar;
 import at.slini.spigotenergyapi.remastered.core.Utils.SConfig;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SpigotEnergyAPI extends JavaPlugin {
 
     private static SpigotEnergyAPI instance;
 
-    public static SConfig config;
+    public static FileConfiguration configtemp;
     public static SConfig RegistertsBlockIds;
 
     public static String prefix;
@@ -52,28 +51,13 @@ public final class SpigotEnergyAPI extends JavaPlugin {
         if (!getDataFolder().exists()) {
             getDataFolder().mkdirs();
         }
-        config = ConfigUtil.getConfig("config");
+
+        configtemp = getConfig();
         RegistertsBlockIds = ConfigUtil.getConfig("RegistertsBlockIds");
 
-        if(!config.getFile().isFile()) {
-            config.setDefault("prefix", "§eSpigotEnergy§7: §r");
-            config.setDefault("energyprefix", "SE");
-            config.setDefault("energyblockupdatefrequenz", 10);
-            config.setDefault("energyshowinactionbar", false);
-
-            // network/steam
-            config.setDefault("network_tick_period", 5);
-            config.setDefault("steam_transfer_enabled", true);
-            config.setDefault("steam_pathfinder_max_nodes", 5000);
-        }
-
-        prefix = config.getString("prefix");
+        prefix = configtemp.getString("prefix");
 
         Bukkit.getPluginManager().registerEvents(new BlockListener(), this);
-        
-        if (config.getBoolean("energyshowinactionbar")) {
-            PlayerActionBar.start();
-        }
 
         BlockWrapperInstance.setWrapper(new BlockWrapperManager());
 
@@ -81,12 +65,12 @@ public final class SpigotEnergyAPI extends JavaPlugin {
         this.moduleRegistry = new ModuleRegistry(this);
         this.steamNetworkEngine = new SteamNetworkEngine(this);
 
-        new EnergyBlockUpdate(this).update(config.getInt("energyblockupdatefrequenz", 10));
+        new EnergyBlockUpdate(this).update(configtemp.getInt("update-frequency", 10));
 
         // Steam transfer tick (optional, can be reworked if not good)
-        if (config.getBoolean("steam_transfer_enabled", true)) {
-            int period = Math.max(1, config.getInt("network_tick_period", 5));
-            int maxNodes = Math.max(250, config.getInt("steam_pathfinder_max_nodes", 5000));
+        if (configtemp.getBoolean("steam_transfer_enabled", true)) {
+            int period = Math.max(1, configtemp.getInt("network_tick_period", 5));
+            int maxNodes = Math.max(250, configtemp.getInt("steam_pathfinder_max_nodes", 5000));
             steamNetworkEngine.start(period, maxNodes);
         }
     }
